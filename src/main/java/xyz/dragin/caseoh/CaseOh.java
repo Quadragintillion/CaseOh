22136818a7f11d4ca6f755737b6d3ecbff471e55
Dragin <dragin@dragin.xyz>
2025-06-15 19:40:04 -0500

    Initial commit

---
+++ src/main/java/xyz/dragin/caseoh/CaseOh.java
@@ -0,0 +1,17 @@
+package xyz.dragin.caseoh;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CaseOh extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
